db39c07abb37a8d30240e4eb7754d6fb98d150f5
Flan-be <cahoy.francisnino@gmail.com>
2026-05-28 07:59:46 +0800

    l

--- Kahoy/settings.py
+++ Kahoy/settings.py
@@ -109,7 +109,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 

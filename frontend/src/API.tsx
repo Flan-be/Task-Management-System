8b825e7d8fd874b1686fcb9c2e9646ba2ee111b1
Flan-be <cahoy.francisnino@gmail.com>
2026-05-28 07:22:48 +0800

    .

--- frontend/src/API.tsx
+++ frontend/src/API.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
-const BASE_URL = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000/api/";
+const BASE_URL = "https://task-management-system-hvnb.onrender.com/api/";
 
 const API = axios.create({
   baseURL: BASE_URL,

3cd1e37511de6b9f56a714daa69e7b7ddb88f166
Flan-be <cahoy.francisnino@gmail.com>
2026-05-28 07:00:51 +0800

    .

---
+++ app/management/commands/createsu.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                email='your@email.com',
+                password='yourpassword'
+            )
